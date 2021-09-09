4ae8d0c7acf2f20a38aa93332b6437636bdcd150
yashhabib01 <yash.d.habib1@gmail.com>
2021-09-08 20:53:27 -0500

    Deploy ready

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-scroll';
+import PortfolioContext from '../../context/context';
+
+import { githubButtons } from '../../mock/data';
+
+const Footer = () => {
+  const { footer } = useContext(PortfolioContext);
+  const { networks } = footer;
+
+  return (
+    <footer className="footer navbar-static-bottom">
+      <Container>
+        <span className="back-to-top">
+          <Link to="hero" smooth duration={1000}>
+            <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+          </Link>
+        </span>
+        <div className="social-links">
+          {networks &&
+            networks.map((network) => {
+              const { id, name, url } = network;
+              return (
+                <a key={id} href={url} rel="noopener noreferrer" target="_blank" aria-label={name}>
+                  <i className={`fa fa-${name} fa-inverse`} />
+                </a>
+              );
+            })}
+        </div>
+        <hr />
+        <p className="footer__text">
+          Made with love by <b>Aniket Habib</b>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/mock/data.js
@@ -0,0 +1,119 @@
+import uuidv1 from 'uuid/v1';
+
+// HEAD DATA
+export const headData = {
+  title: '<Aniket />',
+  lang: '',
+  description: 'Software Engineer',
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: 'Aniket Habib',
+  subtitle: "I'm a Full Stack developer",
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'aniket.jpeg',
+  paragraphOne:
+    "I am a final year enginerring student with majors in Computer Science and a member of student's council. In Addition to coding i have managed a lot of events at highschool.",
+  paragraphTwo:
+    'An energetic and imaginative young developer who is able to work alongside other talented IT professionals in creating websites to the very highest standards ',
+  paragraphThree:
+    'I have tried my hands on a lot of languages. Along with development I practise competitive coding as well.',
+  resume: 'aniket-resume.pdf', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: uuidv1(),
+    img: 'seekho.jpg',
+    title: 'Seekho',
+    info:
+      'Seekho is and online platform to learn languages with help of leitners algorithm and various quizes(test) with a user friendly UI and a complete admin dashboard to manage language(data) and users',
+    info2: '',
+    livetext: '',
+    url: 'https://seekho.netlify.app',
+    repo: 'https://github.com/Aniket2107/seekho-app',
+  },
+  {
+    id: uuidv1(),
+    img: 'shoeclub.jpg',
+    title: 'ShoeCulb',
+    info: 'Full stack E-commerce app and website build with Mern stack with paymentgateway .',
+    info2: '',
+    livetext: '',
+    url: 'https://shoeclub.netlify.app/',
+    repo: 'https://github.com/Aniket2107/mern-Ecommerce-frontend', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'yt.jpg',
+    title: 'Youtube-clone',
+    info:
+      'As the name suggests its a youtube clone build in React.js with react-redux and youtube-v3 api with google authentication using firebase',
+    info2: '',
+    livetext: '',
+    url: 'https://youtube-clone-001.netlify.app/',
+    repo: 'https://github.com/Aniket2107/youtube-clone', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'sort.jpg',
+    title: 'Sorting Visualiser',
+    info: 'A project to visualise the tough and difficult algorithms that students find difficult.',
+    info2: '',
+    livetext: '',
+    url: 'https://aniket2107.github.io/Sorting-visualizer/',
+    repo: 'https://github.com/Aniket2107/Sorting-visualizer', // if no repo, the button will not show up
+  },
+
+  {
+    id: uuidv1(),
+    img: 'covid.jpg',
+    title: 'Covid19 Tracker',
+    info:
+      'Covid19 Tracker helps user aware of rise in covid cases throughout the world. The app has graphs for easy understanding and to help user compare data.',
+    info2: '',
+    livetext: '',
+    url: 'https://aniket2107.github.io/covid-tracker/',
+    repo: 'https://github.com/Aniket2107/covid-tracker', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: 'had096705@gmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: uuidv1(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/anikethabib/',
+    },
+    {
+      id: uuidv1(),
+      name: 'instagram',
+      url: 'https://www.instagram.com/aniket.ts/',
+    },
+    {
+      id: uuidv1(),
+      name: 'github',
+      url: 'https://github.com/Aniket2107',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
